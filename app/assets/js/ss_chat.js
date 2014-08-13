16e961d448ccca708008c2833ad11ebf92117cb2
alexdachin <alex.tm2010@gmail.com>
2014-08-13 15:36:41 +0300

    Right Chat (2)

--- app/assets/js/ss_chat.js
+++ app/assets/js/ss_chat.js
@@ -18,11 +18,84 @@ $('.ss-chat').on('click', '.ss-chat-category', function ()
 	$(this).addClass('is-active');
 	var _new_category = this.getAttribute('data-ss-chat-category');
 
-	$('.ss-chat-category-items:visible').fadeOut(500, function ()
+	$('.ss-chat-category-items:visible').fadeOut(250, function ()
 	{
-		$('.ss-chat-category-items[data-ss-chat-category="' + _new_category + '"]').fadeIn(500, function ()
+		$('.ss-chat-category-items[data-ss-chat-category="' + _new_category + '"]').fadeIn(250, function ()
 		{
 			$('.ss-chat').mCustomScrollbar('update');
 		});
 	});
+});
+
+// User click
+$('.ss-chat').on('click', '.ss-chat-item', function ()
+{
+	var selected_uid = this.getAttribute('data-ss-uid');
+	$('.ss-chat-main').fadeOut(250);
+
+	$.ajax({
+		url: '/ajax/messages/get_conversation',
+		type: 'GET',
+		data: { uid: selected_uid }
+	}).done(function (data) {
+		$('.ss-chat .sent-message, .ss-chat .received-message').not('.ss-chat-conversation-write').remove();
+
+		$.each(data, function (k, v) {
+			var snippet_message = $('.ss-chat .ss-chat-conversation-write').clone().removeClass('ss-chat-conversation-write');
+			snippet_message.removeClass('sent-message');
+			snippet_message.addClass(v.type + '-message');
+			$('.message-content p', snippet_message).text_multiline(v.message);
+			$('.message-time', snippet_message)[0].setAttribute('data-time', v.created_at);
+			$('.message-time', snippet_message).show();
+			$('.message-picture img', snippet_message)[0].setAttribute('src', v.message_picture);
+			$('.ss-chat-conversation-write').before(snippet_message);
+		});
+
+		// Get the color
+		$('.chat-conversation-container').removeClass('is-green');
+		$('.chat-conversation-container').removeClass('is-blue');
+		$('.chat-conversation-container').removeClass('is-red');
+
+		if ($('.ss-chat .ss-chat-category.is-active .ss-chat-category-bull').hasClass('is-red'))
+			$('.chat-conversation-container').addClass('is-red');
+		else if ($('.ss-chat .ss-chat-category.is-active .ss-chat-category-bull').hasClass('is-blue'))
+			$('.chat-conversation-container').addClass('is-blue');
+		else
+			$('.chat-conversation-container').addClass('is-green');
+
+		$('.ss-chat-conversation').fadeIn(250);
+		convert_time();
+		$('.ss-chat').mCustomScrollbar('update');
+		$('.ss-chat').mCustomScrollbar('scrollTo', 'bottom');
+	});
+}).on('click', '.ss-chat-conversation-back', function ()
+{
+	$('.ss-chat-conversation').fadeOut(250, function ()
+	{
+		$('.ss-chat .sent-message textarea').val('');
+		$('.ss-chat-main').fadeIn(250, function ()
+		{
+			$('.ss-chat').mCustomScrollbar('update');
+		});
+	});
+});
+
+// Textarea (send chat message)
+$('.ss-chat').on('keyup', '.sent-message textarea', function (e)
+{
+	while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css('borderTopWidth')) + parseFloat($(this).css('borderBottomWidth')))
+	{
+		$(this).height($(this).height()+10);
+		$('.ss-chat').mCustomScrollbar('update');
+	};
+
+	if (e.keyCode === 13 && $.trim($(this).val()) !== '')
+	{
+		$(this).val('');
+		$(this).height(30);
+	}
+}).on('focus', '.sent-message textarea', function ()
+{
+	// Bugfix that caused scroll to top
+	$('.ss-chat').mCustomScrollbar('scrollTo', this);
 });
--- app/views/snippets/chat.blade.php
+++ app/views/snippets/chat.blade.php
@@ -1,97 +1,111 @@
 <div id="ss-chat-inner" style="display: none;">
-	<h1 class="ss-chat-title">
-		Categories
-	</h1>
+	<div class="ss-chat-main">
+		<h1 class="ss-chat-title">
+			Categories
+		</h1>
 
-	<div class="ss-chat-category is-active" data-ss-chat-category="tutors">
-		<span class="ss-chat-category-bull is-green">&#x25cf;</span>
-		Tutors
-	</div>
+		<div class="ss-chat-category is-active" data-ss-chat-category="tutors">
+			<span class="ss-chat-category-bull is-green">&#x25cf;</span>
+			Tutors
+		</div>
 
-	<div class="ss-chat-category" data-ss-chat-category="friends">
-		<span class="ss-chat-category-bull is-blue">&#x25cf;</span>
-		Friends
-	</div>
+		<div class="ss-chat-category" data-ss-chat-category="friends">
+			<span class="ss-chat-category-bull is-blue">&#x25cf;</span>
+			Friends
+		</div>
 
-	<div class="ss-chat-category" data-ss-chat-category="latest">
-		<span class="ss-chat-category-bull is-red">&#x25cf;</span>
-		Latest Conversations
-	</div>
+		<div class="ss-chat-category" data-ss-chat-category="latest">
+			<span class="ss-chat-category-bull is-red">&#x25cf;</span>
+			Latest Conversations
+		</div>
 
-	{{-- Categories Content --}}
-	<div class="ss-chat-category-items" data-ss-chat-category="tutors">
-		@foreach($chat_tutors as $subject)
-			<h1 class="ss-chat-title is-green">
-				{{{ HTML::limit($subject->name, 18) }}} Tutors
+		{{-- Categories Content --}}
+		<div class="ss-chat-category-items" data-ss-chat-category="tutors">
+			@foreach($chat_tutors as $subject)
+				<h1 class="ss-chat-title is-green">
+					{{{ HTML::limit($subject->name, 18) }}} Tutors
+				</h1>
+
+				<div class="ss-chat-item-list">
+					@foreach ($subject->users['data'] as $user)
+						<div class="ss-chat-item" data-ss-uid="{{{ $user->id }}}">
+							<div class="ss-chat-item-left">
+								<div class="profile-picture">
+									{{ HTML::image(HTML::profile_picture($user), 'Profile Picture', array('width' => 50)) }}
+								</div>
+							</div>
+
+							<div class="ss-chat-item-right">
+								<h2 class="ss-chat-item-name">{{{ HTML::limit($user->name) }}}</h2>
+								<span class="ss-chat-item-info">${{{ $user->price }}}/hour</span>
+							</div>
+							<div class="clear"></div>
+						</div>
+					@endforeach
+				</div>
+			@endforeach
+		</div>
+
+		<div class="ss-chat-category-items" data-ss-chat-category="friends" style="display: none;">
+			<h1 class="ss-chat-title is-blue">
+				Friends
 			</h1>
-
 			<div class="ss-chat-item-list">
-				@foreach ($subject->users['data'] as $user)
-					<div class="ss-chat-item">
+				@foreach ($chat_friends as $friend)
+					<div class="ss-chat-item" data-ss-uid="{{{ $friend->id }}}">
 						<div class="ss-chat-item-left">
 							<div class="profile-picture">
-								{{ HTML::image(HTML::profile_picture($user), 'Profile Picture', array('width' => 50)) }}
+								{{ HTML::image(HTML::profile_picture($friend), 'Profile Picture', array('width' => 50)) }}
 							</div>
 						</div>
 
 						<div class="ss-chat-item-right">
-							<h2 class="ss-chat-item-name">{{{ HTML::limit($user->name) }}}</h2>
-							<span class="ss-chat-item-info">${{{ $user->price }}}/hour</span>
+							<h2 class="ss-chat-item-name">{{{ HTML::limit($friend->name) }}}</h2>
+							<span class="ss-chat-item-info">{{{ $friend->education }}}</span>
 						</div>
 						<div class="clear"></div>
 					</div>
 				@endforeach
 			</div>
-		@endforeach
-	</div>
+		</div>
 
-	<div class="ss-chat-category-items" data-ss-chat-category="friends" style="display: none;">
-		<h1 class="ss-chat-title is-blue">
-			Friends
-		</h1>
-		<div class="ss-chat-item-list">
-			@foreach ($chat_friends as $friend)
-				<div class="ss-chat-item">
-					<div class="ss-chat-item-left">
-						<div class="profile-picture">
-							{{ HTML::image(HTML::profile_picture($friend), 'Profile Picture', array('width' => 50)) }}
+		<div class="ss-chat-category-items" data-ss-chat-category="latest" style="display: none;">
+			<h1 class="ss-chat-title is-red">
+				Latest Conversations
+			</h1>
+			<div class="ss-chat-item-list">
+				@foreach ($chat_conversations as $conversation)
+					<div class="ss-chat-item" data-ss-uid="{{{ $conversation->id }}}">
+						<div class="ss-chat-item-left">
+							<div class="profile-picture">
+								{{ HTML::image(HTML::profile_picture($conversation), 'Profile Picture', array('width' => 50)) }}
+							</div>
 						</div>
-					</div>
 
-					<div class="ss-chat-item-right">
-						<h2 class="ss-chat-item-name">{{{ HTML::limit($friend->name) }}}</h2>
-						<span class="ss-chat-item-info">{{{ $friend->education }}}</span>
+						<div class="ss-chat-item-right" style="margin-top: 0;">
+							<h2 class="ss-chat-item-name">{{{ HTML::limit($conversation->name) }}}</h2>
+							<span class="ss-chat-item-info">
+								<span style="font-size: 12px;" class="time-ago" data-time="{{{ $conversation->created_at }}}"></span>
+								<br />
+								{{{ HTML::limit($conversation->message) }}}
+							</span>
+						</div>
+						<div class="clear"></div>
 					</div>
-					<div class="clear"></div>
-				</div>
-			@endforeach
+				@endforeach
+			</div>
 		</div>
 	</div>
 
-	<div class="ss-chat-category-items" data-ss-chat-category="latest" style="display: none;">
-		<h1 class="ss-chat-title is-red">
-			Latest Conversations
-		</h1>
-		<div class="ss-chat-item-list">
-			@foreach ($chat_conversations as $conversation)
-				<div class="ss-chat-item">
-					<div class="ss-chat-item-left">
-						<div class="profile-picture">
-							{{ HTML::image(HTML::profile_picture($conversation), 'Profile Picture', array('width' => 50)) }}
-						</div>
-					</div>
+	{{-- One conversation --}}
+	<div class="ss-chat-conversation" style="display: none;">
+		<button class="ss-button2 black bold ss-chat-conversation-back">BACK</button>
+		<div class="chat-conversation-container is-red">
+			<!-- Content here -->
 
-					<div class="ss-chat-item-right" style="margin-top: 0;">
-						<h2 class="ss-chat-item-name">{{{ HTML::limit($conversation->name) }}}</h2>
-						<span class="ss-chat-item-info">
-							<span style="font-size: 12px;" class="time-ago" data-time="{{{ $conversation->created_at }}}"></span>
-							<br />
-							{{{ HTML::limit($conversation->message) }}}
-						</span>
-					</div>
-					<div class="clear"></div>
-				</div>
-			@endforeach
+			<div class="sent-message ss-chat-conversation-write">
+				@include('snippets.chat_message')
+			</div>
 		</div>
 	</div>
 </div>
