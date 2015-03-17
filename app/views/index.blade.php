651e20677aa27a0f16249f7b3363e6b60bfda1ac
Alex Dachin <alex.tm2010@gmail.com>
2015-03-17 14:28:10 +0000

    Delete Subscribe box

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -475,31 +475,6 @@
     
     
     
-    <!--Newsletter-->
-     <section class="newsletter">
-     	<div class="container">
-            <div class="row-fluid">
-            	<div class="span12">
-            		<form id="newsletter" class="formNewsletter nm" action="index_submit" method="post" accept-charset="utf-8">
-						<div id="loadingNews" style="display: none" class='alert'>
-			  				<a class='close' data-dismiss='alert'>×</a>Loading
-						</div>	
-		            	<div id="responseNews"></div>
-						<div class="row-fluid">
-							<div class="span12 fields">
-								<label class="left">Stay in touch by email for updates and offers</label>
-								<div class="right">
-									<input type="text" class="large" placeholder="Your email here..." name="Email" value="" />	
-									<input class="btn btn-large btn-primary" type="submit" name="submit" value="SUSCRIBE NOW" id="submit"/>
-								</div>
-							</div>	
-						</div>
-					</form>
-            	</div>
-            </div>
-        </div>
-     </section>
-     <!--End Newsletter-->
     
     <!--Footer-->
     @include('templates.footer')
